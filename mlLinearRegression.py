ba87bf57900e6d2e2e71d3d2f962937bf90b21e3
cphys <22038435+cphys@users.noreply.github.com>
2019-05-28 13:41:01 -0400

    functions are all located in plotWithFitLine.py. Machine learning fit line currently only supports single line regression, currently working on adding multiple line regression as well as non-linear regression

--- mlLinearRegression.py
+++ mlLinearRegression.py
@@ -2,9 +2,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pylab as pl
 import numpy as np
+import os
 from sklearn import linear_model
 from sklearn.metrics import r2_score
+from plotWithFitLine import printListOfHeaders
 from plotWithFitLine import scatPlot
+from plotWithFitLine import plotMLregression
+from plotWithFitLine import histPlots
 #%matplotlib inline
 
 ## Reading in the data and creating a dataframe using pandas ##
@@ -20,90 +24,15 @@ print(df.head(9))
 ## Print a summary of the data set ##
 print(df.describe())
 
-## Creates a list containing all of the column headers in the dataset ##
-headerLabels = df.columns.values
+## Print a list of all of the possible headers in the csv file ##
+printListOfHeaders("example.csv")
 
-
-
-def histPlots(paramList):
-    cdf = df[paramList]
-    ## Visualize the data by creating a histogram of each data set ##
-    cdf.hist()
-    plt.show()
-
-## Select for only the columns that we will be using ##
+## Create a parameter list to be looked at with histogram plots ##
 pL = ['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']
+histPlots(pL,"example.csv")
 
-# histPlots(pL)
+## Plot of Linear regression using sklearn machine learning ##
+plotMLregression('ENGINESIZE','CO2EMISSIONS',"example.csv",saveFile=True)
 
-# scatPlot('FUELCONSUMPTION_COMB','CO2EMISSIONS','example.csv', flOrder = 1)
-# scatPlot('ENGINESIZE','CO2EMISSIONS','example.csv', flOrder = 1)
-# scatPlot('CYLINDERS','CO2EMISSIONS','example.csv', flOrder = 1)
-
-
-########     beginning with machie learning     ########
-#  for the purposes of learning machine learning tools 
-#  we divide the data up into two sets. Here we decide
-#  that we want to use 80% of the data for training and
-#  20% for tessting
-########################################################
-cdf = df[pL]
-msk = np.random.rand(len(df)) < 0.8
-train = cdf[msk]
-test = cdf[~msk]
-
-
-## Plot the training data subset along with the test data subset ##
-pl1 = (train.ENGINESIZE, train.CO2EMISSIONS)
-pl2 =  (test.ENGINESIZE, test.CO2EMISSIONS)
-data = (pl1,pl2)
-colors = ("blue", "red")
-groups = ("Test Data", "Training Data")
-fig = plt.figure()
-ax = fig.add_subplot(111)
-for data, color, group in zip(data, colors, groups):
-    x, y = data
-    ax.scatter(x, y, alpha=0.75, c=color, edgecolors='none', s=30, label=group)
-plt.legend(loc=2)
-plt.show()
-
-
-# Have sklearn calculate a linear regression on the training data ##
-regr = linear_model.LinearRegression()
-train_x = np.asanyarray(train[['ENGINESIZE']])
-train_y = np.asanyarray(train[['CO2EMISSIONS']])
-regr.fit (train_x, train_y)
-# Print the coefficients from the expansion
-print ('Coefficients: ', regr.coef_)
-print ('Intercept: ',regr.intercept_)
-
-# Here we show the results of the linear regression
-plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
-plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
-plt.xlabel("Engine size")
-plt.ylabel("Emission")
-plt.show()
-
-
-# print various error metrics to mesasure the accuracy of our linear model
-test_x = np.asanyarray(test[['ENGINESIZE']])
-test_y = np.asanyarray(test[['CO2EMISSIONS']])
-test_y_ = regr.predict(test_x)
-
-print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
-print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
-print("R2-score: %.2f" % r2_score(test_y_ , test_y) )
-
-# here we show the trainining data in blue, the test data in red
-# with the green line representing a linear fit of all data
-# and the black line representing a linear fit of just the training data
-coefs = np.polyfit(cdf.ENGINESIZE, cdf.CO2EMISSIONS,1)
-plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue', alpha=0.75)
-plt.scatter(test.ENGINESIZE, test.CO2EMISSIONS,  color='red', alpha=0.75)
-plt.plot(train_x,coefs[0]*train_x+coefs[1],'-g')
-plt.plot(train_x, regr.coef_[0,0]*train_x + regr.intercept_[0], '--k')
-plt.xlabel("Engine size")
-plt.ylabel("Emission")
-plt.show()
-
-
+## Plot of Linear regression using numpy values ##
+scatPlot('ENGINESIZE','CO2EMISSIONS',"example.csv",saveFile=True)
--- plotWithFitLine.py
+++ plotWithFitLine.py
@@ -3,14 +3,17 @@ import pandas as pd
 import pylab as pl
 import numpy as np
 import os
+from sklearn import linear_model
+from sklearn.metrics import r2_score
+from matplotlib.offsetbox import AnchoredText
+
 
 def printListOfHeaders(pathToCsv):
     df = pd.read_csv(pathToCsv)
     return print('\nAvalable headers are:\n' + '%s' % '\n'.join(map(str, df.columns.values)) + '\n')
 
 
-
-def scatPlot(xAxis,yAxis, pathToCsv, fitline=True, flOrder = 1, xLabel=True, yLabel=True, scale = 7, ratio = 1, fontSize = 20, saveFile = False, dirName = os.getcwd()):
+def scatPlot(xAxis,yAxis, pathToCsv, fitline=True, flOrder = 1, xLabel=True, yLabel=True, scale = 5, ratio = .75, fontSize = 10, saveFile = False, dirName = os.getcwd(), framePos = 'tl'):
 
     fig = plt.figure(figsize=(scale, ratio * scale))
     ax = fig.add_subplot(111)
@@ -26,7 +29,14 @@ def scatPlot(xAxis,yAxis, pathToCsv, fitline=True, flOrder = 1, xLabel=True, yLa
     except KeyError:
         print('\nError: No column named ' + yAxis + ' is avalable' '\nAvalable columns are:\n' + '%s' % '\n'.join(map(str, headerLabels)) + '\n')
 
-    ax.scatter(xVals, yVals,  color='blue')
+    ax.scatter(xVals, yVals, alpha=0.75,  color='blue',edgecolors = 'none', s=30)
+
+    ## position of data frame window ##
+    #
+    #  need to define other positions
+    if framePos == 'tl':
+        xpos, ypos = [.125,.875]
+
 
     if fitline:
         xValsSorted = np.sort(xVals)
@@ -48,12 +58,12 @@ def scatPlot(xAxis,yAxis, pathToCsv, fitline=True, flOrder = 1, xLabel=True, yLa
 
             fitEqn = str(lin)[2:]
             props = dict(facecolor = 'white', alpha = 0.6, pad = 8) 
-            ax.text(243,53,'$y=$' + fitEqn + '\n$R^2={r:.3f}$'.format(*coefs,r=rSqrd), color='black', fontsize = fontSize, bbox = props)
+            ax.text(xpos * np.max(xVals),ypos * np.max(yVals),'$y=$' + fitEqn + '\n$R^2={r:.3f}$'.format(*coefs,r=rSqrd), color='black', fontsize = fontSize, bbox = props)
 
         else:
             coefs = np.polyfit(xVals, yVals, flOrder)
             lin = np.poly1d(coefs)
-            ax.plot(xValsSorted, lin(xValsSorted), '-g')
+            ax.plot(xValsSorted, lin(xValsSorted), '-r')
  
 
     if xLabel==True:
@@ -79,5 +89,91 @@ def scatPlot(xAxis,yAxis, pathToCsv, fitline=True, flOrder = 1, xLabel=True, yLa
 
 
 
-scatPlot('CO2EMISSIONS','FUELCONSUMPTION_COMB_MPG','example.csv', flOrder = 1, saveFile = True)
+def plotMLregression(xAxis, yAxis, pathToCsv, colors = ("blue", "red"), location=2, scale = 5, ratio = .75, fitline = True, flOrder = 1, xLabel=True, yLabel=True, fontSize = 10, saveFile = False, dirName = os.getcwd()):
+    fig = plt.figure(figsize=(scale, ratio * scale))
+    ax = fig.add_subplot(111)
+    df = pd.read_csv(pathToCsv)
+    try:
+        xVals = df[xAxis]
+    except KeyError:
+        print('\nError: No column named ' + xAxis + ' is avalable' '\nAvalable columns are:\n' + '%s' % '\n'.join(map(str, headerLabels)) + '\n')
+    try:
+        yVals = df[yAxis]
+    except KeyError:
+        print('\nError: No column named ' + yAxis + ' is avalable' '\nAvalable columns are:\n' + '%s' % '\n'.join(map(str, headerLabels)) + '\n')
 
+
+    #  for the purposes of learning machine learning tools 
+    #  we divide the data up into two sets. Here we decide
+    #  that we want to use 80% of the data for training and
+    #  20% for tessting
+    pL = [xAxis,yAxis]
+    cdf = df[[xAxis,yAxis]]
+
+    msk = np.random.rand(len(df)) < 0.8
+    train = cdf[msk]
+    test = cdf[~msk]
+
+    pl1 = (train[xAxis], train[yAxis])
+    pl2 =  (test[xAxis], test[yAxis])
+
+    data = (pl1,pl2)    
+    groups = ("Test Data", "Training Data")
+    ax = fig.add_subplot(111)
+    for data, color, group in zip(data, colors, groups):
+        x, y = data
+        ax.scatter(x, y, alpha=0.75, c=color, edgecolors='none', s=30, label=group)
+    plt.legend(loc=location)
+
+    if fitline:
+        xValsSorted = np.sort(xVals)
+        intOrder = int(flOrder)
+        if intOrder == 1:
+            regr = linear_model.LinearRegression()
+            train_x = np.asanyarray(train[[xAxis]])
+            train_y = np.asanyarray(train[[yAxis]])
+            regr.fit (train_x, train_y)
+
+            test_x = np.asanyarray(test[[xAxis]])
+            test_y = np.asanyarray(test[[yAxis]])
+            test_y_ = regr.predict(test_x)
+
+            # Print the coefficients from the expansion
+            print ('\nCoefficients: ', regr.coef_)
+            print ('Intercept: ',regr.intercept_)
+            # print various error metrics to mesasure the accuracy of our linear model
+            print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+            print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+            print("R2-score: %.2f" % r2_score(test_y_ , test_y) )
+
+            ax.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-k')
+
+        
+
+    if xLabel==True:
+        ax.set_xlabel(str(xAxis).replace("_"," ").title(), fontsize = fontSize)
+    elif xLabel==False:
+        ax.set_xlabel(None)
+    else:
+        ax.set_xlabel(str(xLabel).title(),fontsize = fontSize)
+
+    if yLabel==True:
+        ax.set_ylabel(str(yAxis).replace("_"," ").title(), fontsize=fontSize)
+    elif yLabel==False:
+        ax.set_ylabel(None)
+    else:
+        ax.set_ylabel(str(yLabel).title(), fontsize=fontSize)
+
+    ax.tick_params(axis='both', labelsize=fontSize)
+
+    if saveFile:
+        plt.savefig(os.path.join(dirName, 'mL_{}_vs_{}_fitOrder{}.png'.format(yAxis,xAxis,flOrder)), bbox_inches='tight')
+        
+    plt.show()
+
+def histPlots(paramList,pathToCsv):
+    df = pd.read_csv(pathToCsv)
+    cdf = df[paramList]
+    ## Visualize the data by creating a histogram of each data set ##
+    cdf.hist()
+    plt.show()
